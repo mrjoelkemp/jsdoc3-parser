b0d7822f030077fa06d93e1faa6aaf5e03ad1869
Mike Naughton <michael.d.naughton@gmail.com>
2015-01-20 09:51:48 -0600

    Fix fatal parse error.
    Fix an issue that was occurring when jsdoc echoed output that was not valid JSON. This would cause a fatal error and halt the program, even though the standard approach would be to return the error with the callback.
    
    Invalid output might occur if a file is not documented or if there is an issue opening the file.

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ var execFile = require('child_process').execFile,
  * @param  {String}   filename
  * @param  {Function} cb       ({Object}) -> null - Executed with the AST
  */
-module.exports = function (filename, cb) {
+module.exports = function(filename, cb) {
   // We can't require.resolve('jsdoc')
   // since jsdoc exposes jsdoc & jsdoc.cmd to .bin folder we can use them on any platform
   var cmd = path.join(__dirname, 'node_modules', '.bin', win32 ? 'jsdoc.cmd' : 'jsdoc');
 
-  execFile(cmd, ['-X', filename], { maxBuffer: 5120 * 1024 }, function (error, stdout) {
-    cb(error, JSON.parse(stdout));
+  execFile(cmd, ['-X', filename], { maxBuffer: 5120 * 1024 }, function(error, stdout) {
+    var parsed;
+
+    try {
+      parsed = JSON.parse(stdout);
+    } catch (ex) {
+      parsed = null;
+      error = ex;
+    }
+
+    cb(error, parsed);
   });
 };
