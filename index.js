161b495556b243fe8818a67d5a259c2065d57221
Joel Kemp <mrjoelkemp@gmail.com>
2015-01-20 21:46:49 -0500

    Mocha and jscs for testing

--- index.js
+++ index.js
@@ -9,21 +9,33 @@ var execFile = require('child_process').execFile,
  * @param  {String}   filename
  * @param  {Function} cb       ({Object}) -> null - Executed with the AST
  */
-module.exports = function(filename, cb) {
+function jsdocParser(filename, cb) {
   // We can't require.resolve('jsdoc')
   // since jsdoc exposes jsdoc & jsdoc.cmd to .bin folder we can use them on any platform
   var cmd = path.join(__dirname, 'node_modules', '.bin', win32 ? 'jsdoc.cmd' : 'jsdoc');
 
-  execFile(cmd, ['-X', filename], { maxBuffer: 5120 * 1024 }, function(error, stdout) {
-    var parsed;
+  execFile(cmd, ['-X', filename], { maxBuffer: 5120 * 1024 }, jsdocParser._onComplete.bind(null, cb));
+}
 
-    try {
-      parsed = JSON.parse(stdout);
-    } catch (ex) {
-      parsed = null;
-      error = ex;
-    }
+/**
+ * Exposed for testing
+ *
+ * @private
+ * @param  {Function} cb
+ * @param  {Object}   error
+ * @param  {String}   stdout
+ */
+jsdocParser._onComplete = function(cb, error, stdout) {
+  var parsed;
 
-    cb(error, parsed);
-  });
+  try {
+    parsed = JSON.parse(stdout);
+  } catch (ex) {
+    parsed = null;
+    error = ex;
+  }
+
+  cb(error, parsed);
 };
+
+module.exports = jsdocParser;
